7100d170a8c179242dc1f3a02bf46e8692261b48
muhammad rafi cahyaputra <71932553+Synnefo15@users.noreply.github.com>
2023-06-16 10:49:57 +0700

    add brycpt

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 //jshint esversion:6
 
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 // const encrypt = require('mongoose-encryption');
-const md5 = require('md5');
+// const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -39,39 +41,43 @@ app.get('/register', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-	const newUser = new User({
-		email: req.body.username, // body -> utk ambil dari isian. username adl "name" di htmlNya
-		password: md5(req.body.password),
-	});
-	// newUser.save(function (err) {
-	// 	if (err) {
-	// 		console.log(err);
-	// 	}else{
-	// 		res.render("secrets")
-	// 	}
-	// });
-	newUser
-		.save()
-		.then(function () {
-			res.render('secrets');
-		})
-		.catch(function (err) {
-			console.log(err);
+	bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+		const newUser = new User({
+			email: req.body.username, // body -> utk ambil dari isian. username adl "name" di htmlNya
+			password: hash,
 		});
+		// newUser.save(function (err) { //* old way
+		// 	if (err) {
+		// 		console.log(err);
+		// 	}else{
+		// 		res.render("secrets")
+		// 	}
+		// });
+		newUser
+			.save()
+			.then(function () {
+				res.render('secrets');
+			})
+			.catch(function (err) {
+				console.log(err);
+			});
+	});
 });
 
 app.post('/login', (req, res) => {
 	const username = req.body.username;
-	const password = md5(req.body.password);
+	const password = req.body.password;
 
 	User.findOne({ email: username })
 		.then(function (foundUser) {
 			if (foundUser) {
-				if (foundUser.password === password) {
-					res.render('secrets');
-				} else {
-					res.send(`password salah`);
-				}
+				bcrypt.compare(password, foundUser.password, function (err, result) {
+					if (result === true) {
+						res.render('secrets');
+					} else {
+						res.send(`password salah`);
+					}
+				});
 			} else {
 				res.send(`user tidak ketemu`);
 			}
