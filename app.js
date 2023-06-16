8422cfbf95fe01945cd64f983beecc97ec9bd3d5
muhammad rafi cahyaputra <71932553+Synnefo15@users.noreply.github.com>
2023-06-16 08:48:05 +0700

    dotenv added

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 //jshint esversion:6
 
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 const app = express();
 
+console.log(process.env.API_KEY);
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/userDB', { useNewUrlParser: true });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
 	email: String,
 	password: String,
-};
+});
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model('User', userSchema);
 
@@ -61,19 +66,18 @@ app.post('/login', (req, res) => {
 	User.findOne({ email: username })
 		.then(function (foundUser) {
 			if (foundUser) {
-				if (foundUser.password===password) {
-					res.render("secrets")
-				}else{
+				if (foundUser.password === password) {
+					res.render('secrets');
+				} else {
 					console.log(`password salah`);
 				}
-				
-			}else{
-				console.log(`user tidak ketemu`);
+			} else {
+				res.send(`user tidak ketemu`);
 			}
 		})
 		.catch(function (err) {
 			console.log(err);
-		})
+		});
 });
 
 app.listen(3001, () => {
